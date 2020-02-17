fixed units

main.py
@@ -87,7 +87,7 @@ def sampleHough(xValues,yValues):
     y = m*x + c
 
     # Now downsample the coordinates to only
-    # include cooridnates in the density field domain
+    # include coordinates in the density field domain
 
     # Create some empty arrays
     xIn = []
@@ -155,9 +155,9 @@ def computeVelocity(dens,time):
     """
     print("Computing the turbulent component of the velocity field")
     # construct the magnitude of v field
-    vx = loadObj(testDataDir + "vx_{}".format(time))[:,0,:] * unit_Velocity # cm /s
-    vy = loadObj(testDataDir + "vy_{}".format(time))[:,0,:] * unit_Velocity # cm /s
-    vz = loadObj(testDataDir + "vz_{}".format(time))[:,0,:] * unit_Velocity # cm /s
+    vx = loadObj(testDataDir + "vx_{}".format(time))[:,0,:] # cm /s
+    vy = loadObj(testDataDir + "vy_{}".format(time))[:,0,:] # cm /s
+    vz = loadObj(testDataDir + "vz_{}".format(time))[:,0,:] # cm /s
 
     # calculate the centre of mass velocity vector
     vx_cm = sum(sum(vx * dens)) / sum(sum(dens))
@@ -276,7 +276,7 @@ if __name__ == "__main__":
         dens        = loadObj(testDataDir + "rho_{}".format(time))
 
         # take a slice through (x,y,z=0)
-        dens    = dens[:,0,:] * unit_Density # g / cm^3
+        dens    = dens[:,0,:] # g / cm^3
         s       = np.log(dens / dens.mean())
 
         # include the velocity information if the velocity argument

regionStatistics.py
@@ -39,11 +39,9 @@ if __name__ == "__main__":
     allIDs      = pillarIDDic['all']
     pillarIDDic.pop('all',None)
 
-
     # Histogram of lifetimes
     hist, _         = np.histogram(allIDs, bins=max(allIDs))
     pillarLifeTime  = hist * 10e3 #yrs
-
     plt.figure(dpi=200)
     plt.hist(x=pillarLifeTime,bins=20,normed=True,log=True,color='black',histtype='step')
     plt.xscale('log')
@@ -52,7 +50,7 @@ if __name__ == "__main__":
     plt.tight_layout()
     plt.show()
 
-    # x,y coordinate plot for
+    # Space-time diagrams for each of the pillars
     colorMap = get_cmap(300, name='flag')
     f, ax = plt.subplots(2,1,dpi=200,sharex=True)
     plt.subplots_adjust(left=0.08, bottom=0.12, right=0.97, top=0.97, wspace=-0.05, hspace=0.05)